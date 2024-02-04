9cc0c478d2155e1f3490a2f4d6808ecd312824af
Akshit Sharma <akshits349@gmail.com>
2024-02-05 00:51:16 +0530

    Resolve conflict in Box.js

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -1,36 +1,27 @@
-<<<<<<< HEAD
-export default function Chip({data, selectedmovie, Setselectedmovie }) {
-    const handleClick = () => {
-      if (selectedmovie.includes(data)) {
-       Setselectedmovie((prev) => prev.filter((item) => item !== data));
-      } else {
-        Setselectedmovie((prev) => [...prev, data]);
-      }
-    };
-    return (
-      <div style={{ background: "green", padding: "12px", borderRadius: "50px",height:"20px",width:"100px" }}>
-        {data}
-        &nbsp;&nbsp;
-      <span onClick={handleClick} style={{color:"red"}}> X</span>
-      </div>
-    );
-  }
-=======
-/* eslint-disable react/prop-types */
-export default function Chip({ data, selectedMovies, setSelectedMovies }) {
+export default function Chip({ data, selectedmovie, Setselectedmovie }) {
   const handleClick = () => {
-    if (selectedMovies.includes(data)) {
-      setSelectedMovies((prev) => prev.filter((item) => item !== data));
+    if (selectedmovie.includes(data)) {
+      Setselectedmovie((prev) => prev.filter((item) => item !== data));
     } else {
-      setSelectedMovies((prev) => [...prev, data]);
+      Setselectedmovie((prev) => [...prev, data]);
     }
   };
   return (
-    <div style={{ background: "green", padding: "12px", borderRadius: "12px" }}>
+    <div
+      style={{
+        background: "green",
+        padding: "12px",
+        borderRadius: "50px",
+        height: "20px",
+        width: "100px",
+      }}
+    >
       {data}
       &nbsp;&nbsp;
-      <span onClick={handleClick}> X</span>
+      <span onClick={handleClick} style={{ color: "red" }}>
+        {" "}
+        X
+      </span>
     </div>
   );
 }
->>>>>>> 8de5895e704e1c00d2a276c0bffaa074b2b2b458
--- src/components/MovieBox.jsx
+++ src/components/MovieBox.jsx
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-import React from 'react'
+import React from "react";
 
-const Box = ({data, selectedmovie, Setselectedmovie}) => {
+const Box = ({ data, selectedmovie, Setselectedmovie }) => {
   const handleClick = () => {
     if (selectedmovie.includes(data.id)) {
       Setselectedmovie((prev) => prev.filter((item) => item !== data.id));
@@ -10,47 +9,24 @@ const Box = ({data, selectedmovie, Setselectedmovie}) => {
     }
   };
   return (
-    //here i write the inline css for the movies genre cards
-    <div style={{width:"180px",height:"180px",background:data.color,borderRadius:"12px", border: `${
-      selectedmovie.includes(data.id)
-        ? "4px solid green"
-        : "4px solid black"
-    }`,}} 
-    onClick={handleClick}>
-      <h2>{data.id}</h2>
-      {data.image}
-    </div>
-    
-  )
-}
-
-export default Box
-=======
-/* eslint-disable react/prop-types */
-export default function Box({ data, selectedMovies, setSelectedMovies }) {
-  const handleClick = () => {
-    if (selectedMovies.includes(data.id)) {
-      setSelectedMovies((prev) => prev.filter((item) => item !== data.id));
-    } else {
-      setSelectedMovies((prev) => [...prev, data.id]);
-    }
-  };
-  return (
     <div
       style={{
+        width: "180px",
+        height: "180px",
         background: data.color,
-        textAlign: "center",
+        borderRadius: "12px",
         border: `${
-          selectedMovies.includes(data.id)
+          selectedmovie.includes(data.id)
             ? "4px solid green"
             : "4px solid black"
         }`,
       }}
       onClick={handleClick}
     >
-      <h1>{data.id}</h1>
+      <h2>{data.id}</h2>
       {data.image}
     </div>
   );
-}
->>>>>>> 8de5895e704e1c00d2a276c0bffaa074b2b2b458
+};
+
+export default Box;
